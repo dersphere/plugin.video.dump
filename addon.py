226d3fc5841dddf9a8b54b7e31e5e8016c34b233
dersphere <sphere@dersphere.de>
2013-04-04 00:21:46 +0200

    added new addon.xml tags (website, source, forum, email, license)

--- addon.py
+++ addon.py
@@ -17,7 +17,7 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from xbmcswift2 import Plugin
+from xbmcswift2 import Plugin, xbmc
 from resources.lib import scraper
 
 plugin = Plugin()
